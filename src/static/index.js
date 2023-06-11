6ab5ef044f9b7f7a77aecd0b0ed56ba38b40c7dc
Lukacho <luka.gogiashvili.02@gmail.com>
2023-06-11 17:39:13 +0400

    added images

---
+++ src/static/index.js
@@ -0,0 +1,24 @@
+import tipsandtricks from "./images/tipsandtricks.jpg";
+import howtousesearch from "./images/howtousesearch.jpg";
+import tentricks from "./images/10tricks.jpg";
+import unrealengine from "./images/unrealengine.jpg";
+import programmingforbegginners from "./images/programmingforbegginners.jpg";
+import typescript50 from "./images/typescript50.jpg";
+import programmingtypescript from "./images/programmingtypescript.jpg";
+import tacklingtypescript from "./images/tacklingtypescript.jpg";
+import tsstartguide from "./images/tsstartguide.png";
+import tsexample from "./images/tsexample.jpeg";
+import tsworkshop from "./images/tsworkshop.jpg";
+export {
+  tipsandtricks,
+  howtousesearch,
+  tentricks,
+  unrealengine,
+  programmingforbegginners,
+  typescript50,
+  programmingtypescript,
+  tacklingtypescript,
+  tsstartguide,
+  tsexample,
+  tsworkshop,
+};
